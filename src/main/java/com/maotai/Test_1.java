c5d1ada46cd59ec29485e0d5704c5ed305193d00
陈先锋 <chenxf@moutaiclub.org>
2020-07-23 22:51:52 +0800

    chenxf修改2.0

--- src/main/java/com/maotai/Test_1.java
+++ src/main/java/com/maotai/Test_1.java
@@ -5,7 +5,6 @@ public class Test_1 {
     public static void main(String[] args) {
         System.out.println("先锋大帅哥");
         System.out.println("先锋大帅哥");
-        System.out.println("先锋大帅哥");
     }
 
 }
