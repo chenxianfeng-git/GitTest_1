cb4da704ecbeda29045bd0ef4cf3bd324dba09a8
chenxf <192128698@qq.com>
2020-07-23 22:35:57 +0800

    chenxf修改2.0

--- src/main/java/com/maotai/Test_1.java
+++ src/main/java/com/maotai/Test_1.java
@@ -4,6 +4,10 @@ public class Test_1 {
 
     public static void main(String[] args) {
         System.out.println("先锋大帅哥");
+        System.out.println("先锋大帅哥");
+        System.out.println("先锋大帅哥");
+        System.out.println("先锋大帅哥");
+        System.out.println("先锋大帅哥");
     }
 
 }
